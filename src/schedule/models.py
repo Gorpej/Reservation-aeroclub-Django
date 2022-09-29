5e0d52834fc1fd04f305c2fdd4bd2a5bec37d824
jeremy <aviathur@gmail.com>
2022-09-29 12:51:13 -0400

    dotenv

--- src/schedule/models.py
+++ src/schedule/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Users(models.Model):
     name = models.CharField(max_length=70)
     first_name = models.CharField(max_length=70)
-    email = models.EmailField()
-    num_aero = models.IntegerField()
+    email = models.EmailField(max_length=254,unique=True)
+    num_aero = models.IntegerField(default=0)
     admin = models.BooleanField(default=False)
-    avatar = models.ImageField()
+    #avatar = models.ImageField()
